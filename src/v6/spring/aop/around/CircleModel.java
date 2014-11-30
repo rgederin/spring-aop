be38482f72d01788e83436dac15dd583ed9a0d8d
Ruslan Gederin <gederin.ruslan@gmail.com>
2014-11-30 15:26:14 +0200

    spring aop - around advice

---
+++ src/v6/spring/aop/around/CircleModel.java
@@ -0,0 +1,17 @@
+package v6.spring.aop.around;
+
+public class CircleModel {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setNameWithException(String name) throws Exception {
+		throw( new Exception());	
+	}
+}
